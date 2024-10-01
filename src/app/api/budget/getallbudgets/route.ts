06dc6140e6dc95b4b67956cff114a0ed58cc66ed
Zynalex9 <alzn952@gmail.com>
2024-10-01 15:50:42 +0500

    GET All budgets

---
+++ src/app/api/budget/getallbudgets/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+import budgetModel from "../../../../../models/budgetModel.model";
+import jwt from "jsonwebtoken";
+
+export async function GET(request: NextRequest) {
+  try {
+    const token = request.cookies.get("token");
+    const userId = token?.value;
+
+    if (!userId) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    const decoded = jwt.verify(userId, process.env.TOKEN_SECRET!);
+    const userIdFromToken = (decoded as { id: string }).id;
+
+    const allBudgets = await budgetModel
+      .find({ userId: userIdFromToken })
+      .populate("userId", "displayName email");
+    console.log(userIdFromToken);
+    return NextResponse.json({ success: true, budgets: allBudgets });
+  } catch (error: any) {
+    console.error("Error retrieving budgets:", error);
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Internal Server Error",
+        error: error.message,
+      },
+      { status: 500 }
+    );
+  }
+}
