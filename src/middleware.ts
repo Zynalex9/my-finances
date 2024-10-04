656c0cfd3343e68546e53ea9ba9cda990f12bbf6
Zynalex9 <alzn952@gmail.com>
2024-10-04 12:41:35 +0500

    Middleware

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const tokenData = request.cookies.get("token");
+  const url = request.nextUrl;
+
+  if (!tokenData && url.pathname === "/") {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+
+  if (
+    tokenData &&
+    (url.pathname === "/sign-in" || url.pathname === "/sign-up")
+  ) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/", "/sign-in", "/sign-up"], 
+};
