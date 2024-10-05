4f36f4bfdca17836893c51f8270708a70ca736c6
Zynalex9 <alzn952@gmail.com>
2024-10-05 12:26:14 +0500

    Authentication

--- src/app/(authentication)/sign-in/page.tsx
+++ src/app/(authentication)/sign-in/page.tsx
@@ -1,11 +1,10 @@
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { useToast } from "@/hooks/use-toast"
-
-import { Button } from "@/components/ui/button"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { useToast } from "@/hooks/use-toast";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -14,56 +13,105 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
+// Define the schema for the form validation
 const FormSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
+  email: z.string().email("Invalid email address."),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters.",
   }),
-})
+});
 
-export default  function InputForm() {
-  const { toast } = useToast(); 
+export default function LoginForm() {
+  const { toast } = useToast();
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      username: "",
+      email: "",
+      password: "",
     },
-  })
-
-  function onSubmit(data: z.infer<typeof FormSchema>) {
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-    })
+  });
+  const router = useRouter();
+  async function onSubmit(data: z.infer<typeof FormSchema>) {
+    try {
+      const response = await axios.post("/api/user/sign-in", data);
+      router.replace("/");
+      toast({
+        title: "Logged in successfully",
+        description: `Welcome back ${data.email}`,
+      });
+    } catch (error) {}
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
-  )
+    <div className="bg-gray-900 h-screen w-full flex items-center justify-center p-4">
+      <div className="bg-gray-800 rounded-lg shadow-lg w-full max-w-md p-6 space-y-4">
+        <h1 className="text-2xl font-semibold text-center text-white">
+          Log In
+        </h1>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            {/* Email Field */}
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-gray-200">Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      placeholder="example@mail.com"
+                      {...field}
+                      className="border-gray-600 bg-gray-700 text-gray-200 focus:border-blue-500 focus:ring focus:ring-blue-500"
+                    />
+                  </FormControl>
+                  <FormDescription className="text-gray-400">
+                    Enter the email associated with your account.
+                  </FormDescription>
+                  <FormMessage className="text-red-500" />
+                </FormItem>
+              )}
+            />
+
+            {/* Password Field */}
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-gray-200">Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="********"
+                      {...field}
+                      className="border-gray-600 bg-gray-700 text-gray-200 focus:border-blue-500 focus:ring focus:ring-blue-500"
+                    />
+                  </FormControl>
+                  <FormDescription className="text-gray-400">
+                    Enter your account password.
+                  </FormDescription>
+                  <FormMessage className="text-red-500" />
+                </FormItem>
+              )}
+            />
+
+            {/* Submit Button */}
+            <Button
+              type="submit"
+              className="w-full bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 py-2"
+            >
+              Log In
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
 }
--- src/app/(authentication)/sign-up/page.tsx
+++ src/app/(authentication)/sign-up/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { zodResolver } from "@hookform/resolvers/zod";
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import { Button } from "@/components/ui/button";
 import React from "react";
 import { useForm, FormProvider } from "react-hook-form";
@@ -13,119 +13,126 @@ import {
 import { toast, useToast } from "@/hooks/use-toast";
 import { signUpSchema } from "../../../../schema/signUpSchema";
 import { useRouter } from "next/navigation";
+
 const SignUp = () => {
-  const router = useRouter()
+  const router = useRouter();
   const methods = useForm({
     resolver: zodResolver(signUpSchema),
   });
   const { toast } = useToast();
+
   const onSubmit = async (data: any) => {
     try {
       const response = await axios.post("/api/user/sign-up", data);
       toast({
-        title: 'Success',
+        title: "Success",
         description: response.data.message,
       });
-      router.replace('/sign-in')
-    } catch (error:any) {
-      console.log("error in submitting to sign up", error.message)
+      router.replace("/sign-in");
+    } catch (error: any) {
+      console.log("error in submitting to sign up", error.message);
     }
   };
 
   return (
     <div className="bg-gray-900 h-screen w-full flex items-center justify-center p-4">
-      <div className="bg-white rounded-lg shadow-lg w-full max-w-md p-8 space-y-6">
-        <h1 className="text-2xl font-semibold text-center text-gray-800">
+      <div className="bg-gray-800 rounded-lg shadow-lg w-full max-w-md p-6 space-y-4">
+        <h1 className="text-3xl font-bold text-center text-white">
           Sign Up
         </h1>
 
         <FormProvider {...methods}>
           <form onSubmit={methods.handleSubmit(onSubmit)} className="space-y-4">
-            {/* Username field */}
+            {/* Display Name Field */}
             <FormField
               control={methods.control}
               name="displayName"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel className="text-gray-700">Display Name</FormLabel>
+                  <FormLabel className="text-gray-200">Display Name</FormLabel>
                   <input
                     {...field}
                     type="text"
                     placeholder="Enter your display name"
-                    className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                    className="w-full px-4 py-2 bg-gray-700 border border-gray-600 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none text-white"
                   />
                   <FormMessage />
                 </FormItem>
               )}
             />
 
-            {/* Password field */}
+            {/* Username Field */}
             <FormField
               control={methods.control}
               name="username"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel className="text-gray-700">Username</FormLabel>
+                  <FormLabel className="text-gray-200">Username</FormLabel>
                   <input
                     {...field}
                     type="text"
                     placeholder="Enter your username"
-                    className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                    className="w-full px-4 py-2 bg-gray-700 border border-gray-600 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none text-white"
                   />
                   <FormMessage />
                 </FormItem>
               )}
             />
+
+            {/* Email Field */}
             <FormField
               control={methods.control}
               name="email"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel className="text-gray-700">Email</FormLabel>
+                  <FormLabel className="text-gray-200">Email</FormLabel>
                   <input
                     {...field}
                     type="email"
                     placeholder="Enter your email"
-                    className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                    className="w-full px-4 py-2 bg-gray-700 border border-gray-600 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none text-white"
                   />
                   <FormMessage />
                 </FormItem>
               )}
             />
+
+            {/* Password Field */}
             <FormField
               control={methods.control}
               name="password"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel className="text-gray-700">Password</FormLabel>
+                  <FormLabel className="text-gray-200">Password</FormLabel>
                   <input
                     {...field}
                     type="password"
                     placeholder="Enter your password"
-                    className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                    className="w-full px-4 py-2 bg-gray-700 border border-gray-600 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none text-white"
                   />
                   <FormMessage />
                 </FormItem>
               )}
             />
+
+            {/* Currency Field */}
             <FormField
               control={methods.control}
               name="currency"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel className="text-gray-700">
-                    Your Preferred Currency
-                  </FormLabel>
+                  <FormLabel className="text-gray-200">Preferred Currency</FormLabel>
                   <input
                     {...field}
                     type="text"
-                    placeholder="e.g usd, pkr, inr"
-                    className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                    placeholder="e.g. USD, PKR, INR"
+                    className="w-full px-4 py-2 bg-gray-700 border border-gray-600 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none text-white"
                   />
                   <FormMessage />
                 </FormItem>
               )}
             />
+
             {/* Submit Button */}
             <Button
               disabled={methods.formState.isSubmitting}
