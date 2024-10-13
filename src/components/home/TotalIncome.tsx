b0872637d70259c0c6c3a0c776bd0c8d0a9c8ca0
Zynalex9 <alzn952@gmail.com>
2024-10-13 16:12:43 +0500

    Fixed TotalIncome

--- src/components/home/TotalIncome.tsx
+++ src/components/home/TotalIncome.tsx
@@ -1,14 +1,16 @@
 "use client";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const TotalIncome = () => {
   const [income, setIncome] = useState<number | undefined>(undefined);
-  const [loading, setLoading] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchIncomes = async () => {
       try {
+        setLoading(true);
         const response = await axios.get("/api/income/totalIncome");
         setIncome(response.data.totalIncome);
       } catch (error) {
@@ -21,10 +23,6 @@ const TotalIncome = () => {
     fetchIncomes();
   }, []);
 
-  useEffect(() => {
-    console.log("Income updated:", income);
-  }, [income]);
-
   const formatIncome = (value: number | undefined) => {
     if (value !== undefined) {
       return `$${value.toLocaleString(undefined, {
@@ -36,23 +34,25 @@ const TotalIncome = () => {
   };
 
   return (
-    <div className="w-full mr-5 md:mx-0 md:w-1/3 text-white border bg-gray-800 py-8 px-12 flex flex-col justify-around gap-8 m-1 rounded-lg md:h-80 hover:shadow-2xl transition-shadow	 ">
+    <div className="w-full mr-5 md:mx-0 md:w-1/3 text-white border bg-gray-800 py-8 px-12 flex flex-col justify-around gap-8 m-1 rounded-lg md:h-80 hover:shadow-2xl transition-shadow">
       <div className="section-header">
         <h1 className="text-3xl">Balance</h1>
       </div>
+
       {loading ? (
-        <div>Loading...</div>
+        <Skeleton className="w-24 h-10 bg-gray-700 rounded"></Skeleton> // Adjusted skeleton width/height for a more realistic look
       ) : (
         <div className="balance text-3xl font-semibold">
           {formatIncome(income)}
         </div>
       )}
+
       <div className="footer">
         <p>
-          Available to spend: <strong>$20912.2</strong>{" "}
+          You total income is <strong>{income ? formatIncome(income) : "calculating..."}</strong>.
         </p>
         <p>
-          Total Budgets: <strong>$20912.2</strong>{" "}
+          Keep track of your <strong>financial goals</strong> and plan wisely!
         </p>
       </div>
     </div>
