1dd2aa1246208bbbe432b57bd6e48c3d5d2c9fca
Zynalex9 <alzn952@gmail.com>
2024-10-17 12:28:30 +0500

    Fixed Middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -18,5 +18,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/", "/sign-in", "/sign-up","/add/:*","/all/:*"], 
+  matcher: ["/", "/sign-in", "/sign-up", "/add/:path*", "/all/:path*"], 
 };
